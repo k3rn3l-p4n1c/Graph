c27a998093595385535c12f1a464270af9046908
Bardia Heydari nejad <az.bardia13@gmail.com>
2014-03-20 13:37:38 +0330

    view login...

--- Graph/urls.py
+++ Graph/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Graph.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^polls/', include('login.urls')),
+	url(r'^login/', include('login.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- login/views.py
+++ login/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def login(request):
+    return HttpResponse("Login")
+    
 # Create your views here.
