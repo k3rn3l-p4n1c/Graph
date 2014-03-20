2b0fead0d1ee22b5f66bc0d2bfc4c014b68d6b1a
SoheilKhodayari <s_khodayari@outlook.com>
2014-03-20 12:42:33 +0430

    Just finished the chating/messageing
    Please check the htmls on the template folder  Need SomeOne to Check the
    Correctness Of written files. each html file on messages/templates must
    inherit from the base htmls on templates  main folder SO correct the
    name below at my htmls  :
    % extends "template/...." %  (write the base html file name here )

---
+++ messages/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class MessageForm(forms.Form):
+	subject = forms.CharField(max_length=60)
+	message = forms.CharField(widget=forms.Textarea)
+
+class PublicMessageForm(forms.Form):
+	message = forms.CharField(widget=forms.Textarea,max_length=160)
+	status = "A"
