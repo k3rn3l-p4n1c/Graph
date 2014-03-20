b5bc36f50ab9f58f2e658f1ed8d0f712c7899c76
SoheilKhodayari <s_khodayari@outlook.com>
2014-03-20 13:57:25 +0330

    Create models.py

---
+++ VertexActions/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# vertex rating Actions
+class Rate(models.Model):
+	userid_rater = models.ForeignKey(User,related_name='rater')
+	userid_rated = models.ForeignKey(User,related_name='rated')
+	commend = models.TextField()
+	RATE_CHOICES = (
+		( '1' , '1/5' ),
+		( '2' , '2/5' ),
+		( '3' , '3/5' ),
+		( '4' , '4/5' ),
+		( '5' , '5/5' ),
+	)
+	rate = models.CharField(max_length=1,choices=RATE_CHOICES)
+	
+	def __unicode__(self):
+		return "%s - %s" % (self.userid_rater.username,self.userid_rated.username)
+
+# vertex blocking/abuse reports		
+class AbuseReport(models.Model):
+	userid_rater = models.ForeignKey(User,related_name='arater')
+	userid_rated = models.ForeignKey(User,related_name='arated')
+	commend = models.TextField()
+	STATUS_CHOICES = (
+		( 'O' , 'OPEN' ),
+		( 'C' , 'CLOSED' ),
+	)
+	status = models.CharField(max_length=1,default='O',choices=STATUS_CHOICES)
+	timestamp = models.DateTimeField(auto_now_add='true')
+	
+	def __unicode__(self):
+		return "%s - %s" % (self.userid_rater.username,self.userid_rated.username)
+
+
+# extend this models if possible ?
