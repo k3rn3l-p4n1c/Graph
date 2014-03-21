b1aee97da0bdd43d936714964148777e01473bf8
SoheilKhodayari <s_khodayari@outlook.com>
2014-03-21 21:25:42 +0430

    deleted VertexActions & messages

--- VertexActions/admin.py
+++
@@ -1,6 +0,0 @@
-from VertexActions.models import *
-from django.contrib import admin
-
-admin.site.register(Rate)
-admin.site.register(AbuseReport)
-
--- messages/admin.py
+++
@@ -1,4 +0,0 @@
-from messages.models import Message
-from django.contrib import admin
-
-admin.site.register(Message)
