808c4df8251296fafa5f4b4e52a08209f1946b5c
Bardia Heydari nejad <bardia-heydarinejad@users.noreply.github.com>
2014-03-22 00:06:40 -0700

    undo change

--- create/urls.py
+++ create/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from create import views
 
 urlpatterns = patterns('',
-    url(r'^create/', views.create, name='create')
+    url(r'^/', views.create, name='create')
 )
