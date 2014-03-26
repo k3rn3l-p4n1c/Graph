bace600079db0052885b3c7ac961f06c3a780f95
arian-x <arian@Arians-MacBook-Pro.local>
2014-03-27 02:58:28 +0430

    Flow and Edge added + a flow sharing

--- vertex/models.py
+++ vertex/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-import datetime
 from django.utils import timezone
+import datetime
 
 class Vertex(models.Model,object):
     password = models.CharField(max_length=50)
@@ -11,21 +11,30 @@ class Vertex(models.Model,object):
     tel = models.CharField(max_length=20)
     city = models.CharField(max_length=20)
     country = models.CharField(max_length=20)
-    sex = models.BooleanField() # 1 for male  // 0 for female
+    sex = models.BooleanField(default = 1) # 1 for male  // 0 for female
     birthdate = models.DateField()
     reg_date = models.DateTimeField('date published')
-    #statues = models.CharField(max_length=30)
-    #varificationCode = models.models.CharField(max_length=50)
-    
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.firstname+' '+self.lastname
-        
-class Edge(models.Model,object): 
-	""" tail ==> head """
-	vertex_head = models.ForeignKey(Vertex)
-	vertex_tail = models.ForeignKey(Vertex)
-	def __unicode__(self):  # Python 3: def __str__(self):
-        return self.vertex_tail.user_id+' to '+self.vertex_head.user_id
-	
+
+class Friends(models.Model):
+    name = models.CharField(max_length = 200)
+    members = models.ManyToManyField(Vertex,through = 'Edge')
+    def __unicode__(self):
+        return self.name
+class Edge(models.Model):
+    vertex = models.ForeignKey(Vertex)
+    group = models.ForeignKey(Friends)
+    
+
+
+class Flow(models.Model): 
+    vertexes = models.ManyToManyField(Vertex)
+    text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('date published')
+    
+    def __unicode__(self):
+        return self.text
+    
 # Create your models here. 
 
--- vertex/views.py
+++ vertex/views.py
@@ -5,6 +5,10 @@ from vertex.models import Vertex
 def profile(request, user_id):
 	try:
 		vertex = Vertex.objects.get(user_id=user_id)
+        fname = vertex.firstname
+        lname = vertex.lastname
+        flowlist = vertex.flow_set.all()
+        vertex_friends = Friends.objects.get(name = user_id+'_friends')
 	except :
 		return render_to_response('404error.html',
 			{},
@@ -13,7 +17,11 @@ def profile(request, user_id):
 	try:
 		eml = request.COOKIES[ 'email' ]
 		pwd = request.COOKIES[ 'password' ]
-		client = Vertex.objects.get(email = eml)
+        client = Vertex.objects.get(email = eml)
+        fname = vertex.firstname
+        lname = vertex.lastname
+        flowlist = vertex.flow_set.all()
+        vertex_friends = Friends.objects.get(name = user_id+'_friends')
 		if client.password != pwd:
 			raise LookupError()
 	except:
@@ -32,6 +40,19 @@ def profile(request, user_id):
 			{"VERTEX_DETAIL":vertex.firstname+' '+vertex.lastname},
 			context_instance=RequestContext(request))
 	
-	return HttpResponse("You're looking at vertex %s." % vertex)
+	return HttpResponse("You're looking at vertex %s." % vertex) #making a home page template like facebook home
+
+def flow(request,user_id):
+    vertex = Vertex.objects.get(user_id=user_id)
+    fname = vertex.firstname
+    lname = vertex.lastname
+    vertex_friends = Friends.objects.get(name = userid+'_friends')
+    flowtext = request.POST['flowtext']
+    newflow = FLow.objects.create(text = flowtext,pub_date = timezone.now())
+    newflow.save()
+    for i in vertex_friends.members.all(): #for now to all....I'll put a choice system later
+        i.flow_set.add(newflow)
+        i.save()
+    
 
 # Create your views here.
