b229a486b31547412ba81e072b9d8db0ef9361ec
Bardia Heydari nejad <az.bardia13@gmail.com>
2014-03-26 23:27:46 +0430

    vertex view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response,RequestContext
 from django.http import HttpResponse,HttpResponseRedirect
 from urllib import urlencode
+from time import sleep
+from vertex.models import Vertex
 
 def home(request):
 	print request.COOKIES
@@ -9,6 +11,15 @@ def home(request):
 		pwd = request.COOKIES[ 'password' ]
 	except KeyError:
 		d = {'server_message':"You are not logged in."}
+		query_str = urlencode(d)
+		return HttpResponseRedirect('/login/?'+query_str)
+	try:
+		client = Vertex.objects.get(email = eml)
+		if client.password != pwd:
+			raise LookupError()
+	except LookupError:
+		sleep(3)
+		d = {'server_message':"Wrong username or password."}
 		query_str = urlencode(d)
 		return HttpResponseRedirect('/login/?'+query_str)
 	return render_to_response('home.html',
--- vertex/views.py
+++ vertex/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render_to_response,RequestContext
+from django.http import HttpResponse,HttpResponseRedirect
 from vertex.models import Vertex
    
 def profile(request, user_id):
-
-	vertex = Vertex.objects.get(user_id=user_id)
+	try:
+		vertex = Vertex.objects.get(user_id=user_id)
+	except :
+		return render_to_response('404error.html',
+			{},
+			context_instance=RequestContext(request))
+	client = None
+	try:
+		eml = request.COOKIES[ 'email' ]
+		pwd = request.COOKIES[ 'password' ]
+		client = Vertex.objects.get(email = eml)
+		if client.password != pwd:
+			raise LookupError()
+	except:
+		client = None
+	
+	me = False
+	if client:
+		if client.user_id == vertex.user_id:
+			me = True
+	if me:
+		return render_to_response('vertex.html',
+			{"VERTEX_DETAIL":"yourself"},
+			context_instance=RequestContext(request))
+	else:
+		return render_to_response('vertex.html',
+			{"VERTEX_DETAIL":vertex.firstname+' '+vertex.lastname},
+			context_instance=RequestContext(request))
+	
 	return HttpResponse("You're looking at vertex %s." % vertex)
 
 # Create your views here.
