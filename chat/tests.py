070d16bd555e9b4919e9f252beda3d40004d5e8c
SoheilKhodayari <s_khodayari@outlook.com>
2014-03-26 20:30:07 +0430

    chat available now

--- chat/tests.py
+++ chat/tests.py
@@ -2,7 +2,7 @@
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 
-Replace this with more appropriate tests .
+Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
