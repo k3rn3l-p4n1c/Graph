7879de3d04a231baf44a0bcda20b642f5fca20cf
arian-x <arian@arians-mbp.(none)>
2014-03-31 12:10:46 +0430

    flow updated

--- vertex/views.py
+++ vertex/views.py
@@ -14,40 +14,35 @@ def profile(request, user_id):
 		if client.password != pwd:
 			raise LookupError()
 	except:
-		client = None
-		
+		client = None	
 	try:
-		vertex = Vertex.objects.get(user_id=user_id)
-        
+		vertex = Vertex.objects.get(user_id=user_id)       
 	except :
-		return render_to_response('404error.html',
-			{},
-			context_instance=RequestContext(request))
-    
-    #flows = client.flow_set.order_by('-last_forward_date')[:5] 
+		return render_to_response('404error.html',{},context_instance=RequestContext(request))
+#flows = client.flow_set.order_by('-last_forward_date')[:5] 
 	me = False
 	if client:
 		if client.user_id == vertex.user_id:
 			me = True
-	if request.POST and client and not me:
-        
+	if request.POST and client and not me:       
+
 		try:
 			new_edge = Edge.objects.get(vertex_tail_id = client.user_id,vertex_head_id = user_id)
 		except:
 			new_edge = Edge(vertex_tail_id = client.user_id,vertex_head_id = user_id)
 			new_edge.save()
-	
-	
+			
+			
+	vertex = Vertex.objects.get(user_id = user_id)
+	flows = vertex.flow_set.order_by('-last_forward_date')[:5]
 	if me:
-		return render_to_response('vertex.html',
-			{"VERTEX_DETAIL":"yourself","VERTEX_ID":user_id, "FOLLOWING_VERTEX":vertex.get_following(), "FOLLOWER_VERTEX":vertex.get_followers(), },
-			context_instance=RequestContext(request))
+		return render_to_response('vertex.html',{"VERTEX_DETAIL":"yourself","VERTEX_ID":user_id, "FOLLOWING_VERTEX":vertex.get_following(), "FOLLOWER_VERTEX":vertex.get_followers(),"flows":flows,},context_instance=RequestContext(request))
 	else:
 		return render_to_response('vertex.html',
-			{"VERTEX_DETAIL":vertex.firstname+' '+vertex.lastname,"VERTEX_ID":user_id,"FOLLOWING_VERTEX":vertex.get_following() , "FOLLOWER_VERTEX":vertex.get_followers(), },
-			context_instance=RequestContext(request))
-	
+{"VERTEX_DETAIL":vertex.firstname+' '+vertex.lastname,"VERTEX_ID":user_id,"FOLLOWING_VERTEX":vertex.get_following() , "FOLLOWER_VERTEX":vertex.get_followers(),"flows":flows, },
+context_instance=RequestContext(request))	
 	return HttpResponse("You're looking at vertex %s." % vertex)
+
 def postflow(request,user_id):
     flow_text = request.POST['flow_text']
     pub_date = timezone.now()
